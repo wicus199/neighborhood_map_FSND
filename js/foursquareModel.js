1c208016409794f472eb2b22d070462d4013f558
Wicus <wicus92@gmail.com>
2018-01-23 21:52:54 +0200

    Add new locations and clean up foursquare model

--- js/foursquareModel.js
+++ js/foursquareModel.js
@@ -6,6 +6,3 @@ var foursquareModel = function(data) {
     this.location = ko.observable(data.response.venues[0].location.address);
     this.venueStats = ko.observable(data.response.venues[0].stats.checkinsCount);
 };
-
-// Obviousliy venueName is not going to exist. The response data is passed in.
-// In the response data, only the json response exist
--- js/markerModel.js
+++ js/markerModel.js
@@ -1,22 +1,43 @@
 markers = [
     {
-        name: "Waterkloof Golf Course",
-        position: {lat: -25.792981, lng: 28.225456}
+        name: "Pretoria National Botanical Garden",
+        position: {lat: -25.739471, lng: 28.273266}
     },
     {
-        name: "Domino's Pizza",
-        position: {lat: -25.794245, lng: 28.282067}
+        name: "Church Square Pretoria",
+        position: {lat: -25.746788, lng: 28.186581}
     },
     {
-        name: "Waterkloof Primary School",
-        position: {lat: -25.777942, lng: 28.24076}
+        name: "Voortrekker Monument",
+        position: {lat: -25.776378, lng: 28.175806}
     },
     {
-        name: "DK Burger",
-        position: {lat: -25.790413, lng: 28.223351}
+        name: "Air Force Base Waterkloof",
+        position: {lat: -25.814632, lng: 28.22412}
     },
     {
-        name: "Waterkloof Baptist Church",
-        position: {lat: -25.802213, lng: 28.247793}
+        name: "Palace of Justice Pretoria",
+        position: {lat: -25.745598, lng: 28.18759}
+    },
+    {
+        name: "Loftus Versfeld Stadium",
+        position: {lat: -25.75327, lng: 28.22291}
+    },
+    {
+        name: "University of Pretoria",
+        position: {lat: -25.754549, lng: 28.231448}
+    },
+    {
+        name: "University of South Africa",
+        position: {lat: -25.767659, lng: 28.199264}
+    },
+    {
+        name: "South African Bureau of Standards",
+        position: {lat: -25.769613, lng: 28.212833}
+    },
+    {
+        name: "Groenkloof Nature Reserve",
+        position: {lat: -25.789658, lng: 28.198595}
     }
+
 ];
