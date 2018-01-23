2d274d811fc4f2c5fae9c64e3ee46ca27be86a15
Wicus <wicus92@gmail.com>
2018-01-23 21:37:51 +0200

    Move foursquare and wikipedia api parameters to a separate file
    Created objects containing the api parameters for the foursquare
    and wikipedia api's. These were moved to a separate file to make
    changing the parameters easier.

---
+++ js/linkModel.js
@@ -0,0 +1,22 @@
+fsSearchModel = {
+    searchEndpoint:  'https://api.foursquare.com/v2/venues/search',
+    exploreEndpoint: 'https://api.foursquare.com/v2/venues/explore',
+    errorString:     'The Foursquare API could not be reached, please reload the website',
+    resultLimit:     '10',
+    client_id:       'G0SN05EYUPGJZLV5VZ4KEP4W3RLXVHCUR3ZVMZYLLZAP1RHT',
+    client_secret:   'JIZ1ZUOUCHAHOH1EL0DPMFAV01UK3QFBSAO33GY0XRRUHTTL',
+    version:         '20180112'
+};
+
+wikiModel = {
+    endpoint:       'https://en.wikipedia.org/w/api.php',
+    action:         'opensearch',
+    format:         'json',
+    genericResponse: [["Generic Response"],
+                     ["No Info"],
+                     ["There are no wikipedia info for this marker!"]],
+    error:           [[''],
+                     ['An error occurred'],
+                     ['Failed to reach wikipedia servers'],
+                     ['']]
+};
