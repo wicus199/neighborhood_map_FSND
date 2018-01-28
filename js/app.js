9e4e1fc7dab8e6983c14f35d414f2e858db13f0f
Wicus <wicus92@gmail.com>
2018-01-28 20:33:38 +0200

    Add and edit comments and change drawMarkers() to initMarkers()

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 /**
-* TODO: Initial marker for foursquare, otherwise template gives error
-* TODO: Display foursquare markers in own list that also works with filter
-* TODO: Finish styling and error handling.
-* TODO: Test wiki changes for error handling
-* TODO: Replace vanilla JS with jQuery
+* @description Function runs when Google maps api could not load
+* @param None
+* @returns None
 */
-
 var mapFail = function() {
 
+    var content = '<div id="gErrorDiv"><h2 id="gErrorHeading">Something went wrong when connecting to the Google Servers</h2>'+
+                  '<p id="gErrorPar">Please try again by reloading the page. Sorry for the inconvenience caused.</p></div>';
+    $('#map').html(content);
 };
 
 /**
@@ -22,8 +22,8 @@ var mapSuccess = function() {
     * @returns None
     */
     var viewModel = function() {
-        // Global map variable (drawMarkers and initMap need access)
         var self = this;
+        // Global map variable
         var map;
 
         // Create an array that will store the list of markers
@@ -34,32 +34,40 @@ var mapSuccess = function() {
         this.fsInstance = ko.observable(0);
         // Observable for the wikipedia article
         this.wikiDiv = ko.observable(0);
-
+        // Observable to hold the selected marker
+        this.selectedMarker = ko.observable(0);
+        // Observable to hold the text input from the user for a location search
+        this.searchQuery = ko.observable('');
         // Create an infowindow object to display third party info on the marker
         this.infowindow = new google.maps.InfoWindow();
-        this.selectedMarker = ko.observable(0);
-        this.searchQuery = ko.observable('');
 
         // Function to delete the temporary foursquare markers
+        /**
+        * @description Function will delete temporary markers created for Foursquare suggested venues
+        * @param None
+        * @returns None
+        */
         this.deleteTempMarkers = function() {
             self.tempMarkersList().forEach(function(currentMarker) {
+                // Set the map to null on all the temporary markers
                 currentMarker.setMap(null);
             });
             self.tempMarkersList([]);
-            if ($(window).width() <= 1051) {
+            // If the window is smaller than 1051, a mobile device is used so close the side navigation when the delete button is clicked
+            if ($(window).width() < 1051) {
                 self.closeSideNav();
             }
         };
 
         /**
-        * @description Draws the default markers on the map from the marker model data
+        * @description Draws the default markers on the map from the marker model data and initializes the foursuare and wikipedia api
         * @param None
         * @returns None
         */
-        this.drawMarkers = function() {
+        this.initMarkers = function() {
             // Iterate through the model containing location info
             markers.forEach(function(currentMarker) {
-                // Create a marker using the Google maps api
+                // Create a marker per location using the Google maps api
                 var marker = new google.maps.Marker({
                     position: currentMarker.position,
                     map: map,
@@ -84,6 +92,8 @@ var mapSuccess = function() {
             });
             // Sets the initial marker to the first marker in the array
             self.selectedMarker(self.markersList()[0].marker);
+            // Pass the first marker to the populateWindows function to initialize the page with wikipedia and foursquare info
+            self.populateWindows(self.markersList()[0].marker);
         };
 
         /**
@@ -92,7 +102,7 @@ var mapSuccess = function() {
         * @returns None
         */
         this.initMap = function() {
-            // This initializes the Map at a certain location
+            // This initializes the Map at the specified location
             map = new google.maps.Map(document.getElementById('map'), {
                 center: {lat: -25.814474, lng: 28.244502},
                 zoom: 13,
@@ -101,7 +111,7 @@ var mapSuccess = function() {
         };
 
         /**
-        * @description Initializes the Google infowindow and creates a Knockout binding
+        * @description Initializes the Google infowindow and creates a Knockout binding on the first run
         * @param None
         * @returns None
         */
@@ -115,14 +125,14 @@ var mapSuccess = function() {
             this.contentString = '<div id="infoWindow"' +
                                 'data-bind="template: {name: \'fs-template\', data: fsInstance}">' +
                                 '</div>';
-
+            // Set the content of the infowindow to the contentString with knockout data bindings
             self.infowindow.setContent(this.contentString);
-
+            // Add a listener to listen for when the DOM is ready (finished loading)
             google.maps.event.addListener(self.infowindow, 'domready', function() {
-                // This will only execute once when the windowLoaded boolean is false
+                // This will only execute once at the start when the windowLoaded boolean is false
                 if (!windowLoaded) {
-                    // Apply a binding to the infowindow
                     windowLoaded = true;
+                    // Apply a binding to the infowindow
                     ko.applyBindings(self, $('#infoWindow')[0]);
                 }
             });
@@ -193,21 +203,25 @@ var mapSuccess = function() {
         };
 
         /**
-        * @description Search wikipedia info for the marker location.
-        * @param {object} marker - Marker object
+        * @description Search wikipedia info for the marker location using the wikipedia api
+        * @param {object} marker - Current marker object
+        * @param {boolean} getWiki - Boolean to determine if wikipedia should search for marker or just the city
         * @returns None
         */
         this.getWikipediaInfo = function(marker, getWiki) {
-
             // Create url with paramenters to search wikipedia
+            // Search parameter will be marker title if getWiki is true and Pretoria if getWiki is false
             var wikiUrl = wikiModel.endpoint + '?' + $.param({
                 'action': wikiModel.action,
-                'search': getWiki ? marker.title : 'Pretoria',  //'search': marker.title,
+                'search': getWiki ? marker.title : 'Pretoria',
                 'format': wikiModel.format
             });
-            // Start a timeout function to timeout after 5 seconds if wikipedia
-            // query does not return a successful response
+            /*
+            * Start a timeout function to timeout after 5 seconds if wikipedia
+            * query does not return a successful response
+            */
             var wikiTimeout = setTimeout(function() {
+                // If timeout occurs, load wikipedia div with error string
                 self.wikiDiv(wikiModel.error);
             }, 5000);
 
@@ -231,11 +245,11 @@ var mapSuccess = function() {
 
         /**
         * @description Get Foursquare info on the selected marker
-        * @param {object} marker - Marker object
+        * @param {object} marker - Currently selected marker object
         * @returns None
         */
         this.getFoursquareInfo = function(marker) {
-            // Search the venue of the marker selected
+            // Search the venue for the selected marker
             // Add the lat and lng parameters to the api endpoint to search location
             var searchUrl = fsSearchModel.searchEndpoint + '?' + $.param({
                 'll' : marker.position.lat() + ',' + marker.position.lng(),
@@ -247,18 +261,25 @@ var mapSuccess = function() {
 
             // Ajax request for fetching foursquare info
             $.getJSON(searchUrl, function(responseData) {
+                // Create a new foursquare instance by creating a new foursquare model by passing the response data to it
                 self.fsInstance(new foursquareModel(responseData));
-            // Function if ajax request fails
+            // Error handler if ajax request fails
             }).fail(function(xhrObject, status, error) {
+                // Set the content of the infowindow to a foursquare error string
                 self.infowindow.setContent(fsSearchModel.errorString);
+                // Pop up an alert box with the error string
                 alert(fsSearchModel.errorString);
             });
         };
 
+        /**
+        * @description Get suggested venues using the selected marker and the Foursquare api
+        * @param {object} marker - Currently selected marker object
+        * @returns None
+        */
         this.createFoursquareMarkers = function(marker) {
-            this.foursquareExplore = 'https://api.foursquare.com/v2/venues/explore';
-
-            var exploreUrl = this.foursquareExplore + '?' + $.param({
+            // Url to search foursquare for suggested venues
+            var exploreUrl = fsSearchModel.exploreEndpoint + '?' + $.param({
                 'll': marker.position.lat() + ',' + marker.position.lng(),
                 'limit': fsSearchModel.resultLimit,
                 'client_id': fsSearchModel.client_id,
@@ -268,57 +289,59 @@ var mapSuccess = function() {
 
             // Ajax request to fetch foursquare info
             $.getJSON(exploreUrl, function(responseData) {
-                console.log(responseData);
                 // First, delete the temporary markers if there were any
                 self.deleteTempMarkers();
                 // Loop through the venues in the response
                 responseData.response.groups[0].items.forEach(function(currentItem,index) {
-                    // Create new markers for the foursuare suggested venues
+                    // Create new markers for the foursuare suggested venues with a different icon
                     var fsMarker = new google.maps.Marker({
                         position: {lat: currentItem.venue.location.lat, lng: currentItem.venue.location.lng},
                         map: map,
                         animation: google.maps.Animation.DROP,
                         title: currentItem.venue.name,
-                        icon: {
-                            path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-                            scale: 5
-                        }
+                        icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
                     });
                     // Push the marker into the temporary markers list
                     self.tempMarkersList.push(fsMarker);
-                    // Add a listener to when a marker is clicked
+                    // Add a listener to when a marker is clicked to select the marker
                     fsMarker.addListener('click', function() {
                         self.selectMarker(fsMarker);
                     });
                 });
+                // Close the infowindow to better see the new foursuare markers
                 self.infowindow.close();
+                // If the window width is smaller than 1051px (mobile), close the side navigation menu for a better user experience
                 if ($(window).width() <= 1051) {
                     self.closeSideNav();
                 }
+            // Error handler to run when the request fails
             }).fail(function(xhrObject, status, error) {
+                // Alert the user with an error string
                 alert(fsSearchModel.errorString);
             });
         };
 
         /*
-        * Computed observable that uses observable array created at the start
+        * Computed observable that uses observable array created at the start to provide search functionality
         * Take query string to lower case in order to always match characters
-        * Computed observable to provide search functionality
         */
         this.result = ko.computed(function() {
             // Returns the result if it is true
             return self.markersList().filter(function(currentVal) {
                 // Will return true if the letters are present in the correct sequence in location
                 if (currentVal.name.toLowerCase().indexOf(self.searchQuery().toLowerCase()) >= 0) {
+                    // If the search query is present in the location title, set the map on the marker to map
                     currentVal.marker.setMap(map);
                     return true;
                 } else {
+                    // If the search query is not present in the location title, set the map on the marker to null
                     currentVal.marker.setMap(null);
                     return false;
                 }
             });
         });
 
+        // Same as the other computed observable result, except for foursquare markers
         this.resultFs = ko.computed(function() {
             // Returns the result if it is true
             return self.tempMarkersList().filter(function(currentVal) {
@@ -333,10 +356,12 @@ var mapSuccess = function() {
             });
         });
 
+        // Function to open the side navigation when the menu button is clicked
         this.openSideNav = function() {
             $('#sidebar')[0].style.width = "250px";
         };
 
+        // Function to close the side navigation menu when the close menu button is clicked
         this.closeSideNav = function() {
             $('#sidebar')[0].style.width = "0px";
             $('#map')[0].style.width = '100%';
@@ -346,9 +371,11 @@ var mapSuccess = function() {
 
         // Initialize the google map on the page
         this.initMap();
+        // Initialize the infoWindow
         this.initInfowindow();
         // Draw the default markers on the page
-        this.drawMarkers();
+        this.initMarkers();
     };
+    // Apply the bindings to the viewModel
     ko.applyBindings(new viewModel());
 };
